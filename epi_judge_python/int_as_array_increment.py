7026b323a56ea8ec848a16de369e93691ca7dabd
felipestawinski <felipe.stawinski@gmail.com>
2025-04-08 15:50:59 -0300

    solved int_as_array_increment

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -5,6 +5,30 @@ from test_framework import generic_test
 
 def plus_one(A: List[int]) -> List[int]:
     # TODO - you fill in here.
+    if A[-1] != 9:
+        A[-1] += 1
+        return A
+    else:
+        x = 1
+        flag = 0
+        for i in reversed(range(len(A))):
+            curr_sum = A[i] + x
+            print(f"curr_sum: {curr_sum}")
+            if curr_sum == 10:
+                A[i] = 0
+                x = 1
+                flag += 1
+            else:
+                A[i] = curr_sum
+                x = 0
+                break
+        if flag == len(A):
+            return [1] + A
+
+    return A
+
+
+
     return []
 
 

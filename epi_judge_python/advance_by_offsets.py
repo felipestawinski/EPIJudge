9ec018781743d2cb52a7d905b65aa856e8e1e156
felipestawinski <felipe.stawinski@gmail.com>
2025-04-13 13:43:12 -0300

    solved advance_by_offsets

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -4,8 +4,24 @@ from test_framework import generic_test
 
 
 def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
+    if len(A) == 1: return True
+    for i in range(len(A)):
+        if A[i] == 0:
+            counter = 1
+            for j in range(i-1, -1, -1):
+                if i == len(A)-1:
+                    if A[j] >= counter:
+                        break
+                    
+                if A[j] > counter:
+                    break
+        
+                counter += 1
+            if counter == i+1:
+                return False
+            
     return True
+               
 
 
 if __name__ == '__main__':
--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -5,6 +5,14 @@ from test_framework import generic_test
 
 def multiply(num1: List[int], num2: List[int]) -> List[int]:
     # TODO - you fill in here.
+    sign = -1 if num1[0] < 0 or num2[0] < 0 else 1
+    int_num = 1
+    m = 1
+
+    for n1 in reversed(range(num1)):
+        m = 1
+        for n2 in reversed(range(num2)):
+            int_num += num2[n2] * num1[n1] * m
     return []
 
 
